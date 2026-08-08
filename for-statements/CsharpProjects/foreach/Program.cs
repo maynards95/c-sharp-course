79849a80072eb12c2e1646238bac587381ae14ae
Ezra Maynard <maynard.c.ezra@gmail.com>
2026-08-08 13:07:46 -0500

    For, foreach with arrays, FizzBuzz exercises

---
+++ for-statements/CsharpProjects/foreach/Program.cs
@@ -0,0 +1,11 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    Console.Write(i);
+    if (i % 3 == 0 && i % 5 == 0)
+        Console.Write(" - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.Write(" - Fizz");
+    else if (i % 5 == 0)
+        Console.Write(" - Buzz");
+    Console.WriteLine();
+}
